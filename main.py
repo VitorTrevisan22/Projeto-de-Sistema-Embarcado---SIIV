cb6084909861dcc3393ff061576dbb3f27792464
Vitor Trevisan <vitortrevisan.mog6@gmail.com>
2026-03-25 23:21:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 import cv2
 import numpy as np
 
-# ----------- CAMERA EXTERNA -----------
-camera_id = 1   # se não abrir, troque para 0, 2 ou 3
+camera_id = 1  
 video = cv2.VideoCapture(camera_id)
 
-# Deixar a câmera mais aberta (resolução maior)
-video.set(3, 1920)  # largura
-video.set(4, 1080)  # altura
+video.set(3, 1920) 
+video.set(4, 1080)  
 
-# ----------- ZOOM DIGITAL (ABRIR IMAGEM) -----------
-ZOOM = 0.75   # 0.75 = imagem mais aberta | 1.0 = normal
+ZOOM = 0.75  
 
-# ----------- VAGAS SEPARADAS -----------
-# Ajustado para ficar igual a sua placa, 3 em cima e 3 em baixo
 
 vaga1 = [205, 130, 260, 100]
 vaga2 = [550, 125, 260, 105]
@@ -25,7 +20,6 @@ vaga6 = [970, 550, 360, 160]
 
 vagas = [vaga1, vaga2, vaga3, vaga4, vaga5, vaga6]
 
-# ----------- TELA CHEIA -----------
 cv2.namedWindow("Estacionamento", cv2.WINDOW_NORMAL)
 cv2.setWindowProperty("Estacionamento", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
@@ -36,7 +30,6 @@ while True:
         print("Erro ao acessar a webcam externa.")
         break
 
-    # Zoom digital para abrir mais a imagem
     h, w = img.shape[:2]
     img = cv2.resize(img, (int(w * ZOOM), int(h * ZOOM)))
 
@@ -50,31 +43,30 @@ while True:
 
     vagasLivres = 0
 
-    # ----------- LOOP DAS 6 VAGAS -----------
     for x, y, w, h in vagas:
         recorte = imgDil[y:y+h, x:x+w]
         branco = cv2.countNonZero(recorte)
 
-        # Mostra valor para depuração
+    
         cv2.putText(img, str(branco), (x, y+h-10),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,255,255), 2)
 
-        # Se muitos pixels brancos = carro
+
         if branco > 3000:
             cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 3)
         else:
             vagasLivres += 1
             cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 3)
 
-    # Caixa superior
+
     cv2.rectangle(img, (10, 10), (400, 70), (255, 0, 0), -1)
     cv2.putText(img, f"LIVRE: {vagasLivres}/6", (25, 55),
                 cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255,255,255), 4)
 
-    # Mostrar imagem final
+
     cv2.imshow("Estacionamento", img)
 
-    # Pressionar "q" para sair
+
     if cv2.waitKey(5) & 0xFF == ord('q'):
         break
 
